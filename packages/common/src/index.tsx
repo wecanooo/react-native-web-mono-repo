c6d8a2e0aacd0baa882bc34a97365359ef50162e
wecanooo <wecanooo@gmail.com>
2020-02-17 23:21:56 +0900

    SafeAreaView 적용

--- packages/common/src/index.tsx
+++ packages/common/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useContext } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, SafeAreaView } from 'react-native';
 import { CounterStoreContext } from './stores/CounterStore';
 import { observer } from 'mobx-react-lite';
 
@@ -8,20 +8,24 @@ export const App = observer(() => {
   const counterStore = useContext(CounterStoreContext);
 
   return (
-    <View style={styles.container}>
-      <Text>Hello, World</Text>
-      <Text>{counterStore.count}</Text>
-      <Button title="increment" onPress={() => counterStore.increment()} />
-      <Button title="decrement" onPress={() => counterStore.decrement()} />
-    </View>
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <View style={styles.container}>
+        <Text>Hello, World</Text>
+        <Text>{counterStore.count}</Text>
+        <Button title="increment" onPress={() => counterStore.increment()} />
+        <Button title="decrement" onPress={() => counterStore.decrement()} />
+      </View>
+    </SafeAreaView>
   );
 });
 
 const styles = StyleSheet.create({
+  safeAreaContainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    height: '100%',
   },
 });
